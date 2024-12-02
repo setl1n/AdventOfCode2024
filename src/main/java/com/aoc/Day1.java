e47cc0fd405ea5833887ec924e46d07f780cc2bc
setl1n <kaungsetlin2008@gmail.com>
2024-12-02 17:31:19 +0800

    day1part2

--- src/main/java/com/aoc/Day1.java
+++ src/main/java/com/aoc/Day1.java
@@ -3,7 +3,9 @@ package com.aoc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -13,8 +15,12 @@ public class Day1 {
             List<String> sample = Files.readAllLines(Paths.get("src/main/resources/day1/sample.txt"));
             List<String> input = Files.readAllLines(Paths.get("src/main/resources/day1/input.txt"));
 
-            System.out.println("Part 1 Answer (for example): " + solvePart1(sample));
-            System.out.println("Part 1 Answer (for example): " + solvePart1(input));
+            System.out.println();
+            System.out.println("Part 1 Answer (example): " + solvePart1(sample));
+            System.out.println("Part 1 Answer: " + solvePart1(input));
+            System.out.println();
+            System.out.println("Part 2 Answer (example): " + solvePart2(sample));
+            System.out.println("Part 2 Answer: " + solvePart2(input));
         } catch (IOException e) {
             System.err.println("Error reading input file: " + e.getMessage());
         }
@@ -31,6 +37,23 @@ public class Day1 {
         long sum = 0;
         while (!list1.isEmpty()) {
             sum += Math.abs(list1.poll() - list2.poll());
+        }
+        return sum;
+    }
+
+    private static long solvePart2(List<String> data) {
+        List<Integer> leftList = new ArrayList<>();
+        HashMap<Integer,Integer> rightList = new HashMap<>();
+        for (String line : data) {
+            String[] inputs = line.split("   ");
+            leftList.add(Integer.parseInt(inputs[0]));
+            int numOnRightList = Integer.parseInt(inputs[1]);
+            // increment count of numbers on right list
+            rightList.put(numOnRightList, rightList.getOrDefault(numOnRightList, 0) + 1);
+        }
+        long sum = 0;
+        for (Integer integer : leftList) {
+            sum += integer * rightList.getOrDefault(integer, 0);
         }
         return sum;
     }
