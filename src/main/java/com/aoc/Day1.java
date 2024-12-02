e0e3d8ea36e3d447ec9c7087fb6b786ed7bbae5e
setl1n <kaungsetlin2008@gmail.com>
2024-12-02 17:23:46 +0800

    day1part1

--- src/main/java/com/aoc/Day1.java
+++ src/main/java/com/aoc/Day1.java
@@ -3,23 +3,35 @@ package com.aoc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Day1 {
     public static void main(String[] args) {
         try {
-            List<String> input = Files.readAllLines(Paths.get("src/main/resources/day1/input.txt"));
             List<String> sample = Files.readAllLines(Paths.get("src/main/resources/day1/sample.txt"));
+            List<String> input = Files.readAllLines(Paths.get("src/main/resources/day1/input.txt"));
 
-            System.out.println("Sample Output: " + solve(sample));
-            System.out.println("Actual Output: " + solve(input));
+            System.out.println("Part 1 Answer (for example): " + solvePart1(sample));
+            System.out.println("Part 1 Answer (for example): " + solvePart1(input));
         } catch (IOException e) {
             System.err.println("Error reading input file: " + e.getMessage());
         }
     }
 
-    private static int solve(List<String> data) {
-        // Example solution: Sum integers in the input
-        return data.stream().mapToInt(Integer::parseInt).sum();
+    private static long solvePart1(List<String> data) {
+        PriorityQueue<Integer> list1 = new PriorityQueue<>();
+        PriorityQueue<Integer> list2 = new PriorityQueue<>();
+        for (String line : data) {
+            String[] inputs = line.split("   ");
+            list1.add(Integer.parseInt(inputs[0]));
+            list2.add(Integer.parseInt(inputs[1]));
+        }
+        long sum = 0;
+        while (!list1.isEmpty()) {
+            sum += Math.abs(list1.poll() - list2.poll());
+        }
+        return sum;
     }
 }
