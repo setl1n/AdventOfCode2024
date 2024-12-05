2280e3ebb177bff43985720a1fef902b862db7f5
setl1n <kaungsetlin2008@gmail.com>
2024-12-05 15:22:07 +0800

    day5part2

--- src/main/java/com/aoc/Day5.java
+++ src/main/java/com/aoc/Day5.java
@@ -19,8 +19,8 @@ public class Day5 {
             System.out.println("Part 1 Answer (example): " + solvePart1(sample));
             System.out.println("Part 1 Answer: " + solvePart1(input));
             System.out.println();
-            // System.out.println("Part 2 Answer (example): " + solvePart2(sample));
-            // System.out.println("Part 2 Answer: " + solvePart2(input));
+            System.out.println("Part 2 Answer (example): " + solvePart2(sample));
+            System.out.println("Part 2 Answer: " + solvePart2(input));
         } catch (IOException e) {
             System.err.println("Error reading input file: " + e.getMessage());
         }
@@ -70,9 +70,46 @@ public class Day5 {
         return sum;
     }
 
-    private static long solvePart2(List<String> data) {
-        int count = 0;
-        return count;
+    private static void swapOutOfOrder(List<Integer> pages, HashMap<Integer,HashSet<Integer>> rules) {
+        List<Integer> occurredBefore = new ArrayList<>();
+        for (int i = 0; i < pages.size(); i++) {
+            int curr = pages.get(i);
+            for (int j = 0; j < occurredBefore.size(); j++) {
+                int prevInt = occurredBefore.get(j);
+                if (rules.getOrDefault(curr, new HashSet<>()).contains(prevInt)) {
+                    pages.add(j,curr);
+                    pages.remove(j+1);
+                    pages.add(i,prevInt);
+                    pages.remove(i+1);
+                    return;
+                }
+            }
+            occurredBefore.add(curr);
+        }
+    }
+
+    private static int reOrderAndGetMiddlePage(List<Integer> pages, HashMap<Integer,HashSet<Integer>> rules) {
+        while (!isValidOrder(pages, rules)) {
+            swapOutOfOrder(pages,rules);
+        }
+        return pages.get(pages.size() / 2);
+    }
+
+    private static long solvePart2(String data) {
+        int sum = 0;
+        HashMap<Integer,HashSet<Integer>> rules = new HashMap<>();
+        List<String> lines = new ArrayList<>();
+        processInput(data, rules, lines);
+        for (String string : lines) {
+            List<Integer> pages = new ArrayList<>();
+            for (String page : string.split(",")) {
+                pages.add(Integer.parseInt(page));
+            }
+            if (!isValidOrder(pages, rules)) {
+                sum += reOrderAndGetMiddlePage(pages, rules);
+            }
+        }
+        return sum;
     }
 
 }
